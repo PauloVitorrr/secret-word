7129d3e97773ccfca9708640c9cb05f84da4467f
Paulo Vitor <paulovitorvaztst@gmail.com>
2022-12-25 18:49:10 -0300

    funções para alternar estágios do jogo

--- src/App.js
+++ src/App.js
@@ -16,19 +16,32 @@ const stages = [
   {id: 1, name: "start"},
   {id: 2, name: "game"},
   {id: 3, name: "end"},
-]
+] 
 
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name)
   const [words] = useState(wordsList)
 
-  console.log(words)
+  // start
+  const startGame = () =>{
+    setGameStage(stages[1].name)
+  }
+
+  //  process the letter
+  const verifyLetter = () => {
+    setGameStage(stages[2].name)
+  }
+
+  // finish latter
+  const retry = () => {
+    setGameStage(stages[0].name)
+  }
 
   return (
     <div className="App">
-      {gameStage === "start" && <StartScreen/>}
-      {gameStage === "game" && <Game/>}
-      {gameStage === "end" && <GameOver/>}
+      {gameStage === "start" && <StartScreen startGame={startGame}/>}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter}/>}
+      {gameStage === "end" && <GameOver retry={retry}/>}
     </div>
   );
 }
